f9d2269a5128fa7f4c3ebb9db10f6f0e158bbe98
Tii <mail@tii.one>
2024-09-04 21:58:33 +0200

    Logo smaller

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -35,7 +35,7 @@ const userNavigation: Array<{name: string, href: string, method: Method|undefine
                     <div class="flex items-center">
                         <div class="flex-shrink-0">
                             <Link href="/" class="flex items-center">
-                                <img class="h-14" src="/images/ffhausen-logo.png" aria-hidden="true"/>
+                                <img class="h-10" src="/images/ffhausen-logo.png" aria-hidden="true"/>
                                 <div class="hidden md:block ml-2 flex flex-col justify-center text-white font-black leading-tight text-justify">
                                     <div>Freiwillige Feuerwehr</div>
                                     <div>Frankfurt Hausen</div>
@@ -68,7 +68,7 @@ const userNavigation: Array<{name: string, href: string, method: Method|undefine
                                 <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
                                     <MenuItems class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                                         <MenuItem v-for="item in userNavigation" :key="item.name" v-slot="{ active }">
-                                            <Link :href="item.href" :method="item.method" as="button" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">{{ item.name }}</Link>
+                                            <Link :href="item.href" :method="item.method" as="button" type="button" :class="[active ? 'bg-gray-100' : '', 'block w-full text-left px-4 py-2 text-sm text-gray-700']">{{ item.name }}</Link>
                                         </MenuItem>
                                     </MenuItems>
                                 </transition>
